d3a8cf14067cbb26fbf4f5383f1d11f2488c087a
魏云龙 <1131093754@qq.com>
2020-03-25 21:36:03 +0800

    '用户列表'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -22,7 +22,12 @@ import {
   Switch,
   Tooltip,
   Pagination,
-  Dialog
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -47,4 +52,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
